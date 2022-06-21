e3e3cdc73bff00305b2856a9647783b527a8a221
sokmean-mptc <ngonsokmean@mptc.gov.kh>
2022-06-21 12:02:07 +0700

    #

--- pages/posts.jsx
+++ pages/posts.jsx
@@ -9,8 +9,6 @@ import { getAllPostsForHome } from '../lib/api';
 import { CMS_NAME } from '../lib/constants';
 
 export default function Index({ allPosts: { edges }, preview }) {
-  const heroPost = edges[0]?.node;
-
   return (
     <Layout preview={preview}>
       <Head>
@@ -28,77 +26,23 @@ export default function Index({ allPosts: { edges }, preview }) {
             className="text-xl font-bold"
           />
         </div>
-        <section className="grid md:grid-cols-2 gap-5 mb-7 md:mb-7">
-          <PostItem
-            post={heroPost}
-            config={{
-              showExcerpt: false,
-              imageHeight: 800,
-              imageWidth: 1000,
-            }}
-            styles={{}}
-          />
-          <div className="grid grid-cols-1 md:grid-cols-2 gap-5">
-            <PostItem
-              key={heroPost.databaseId}
-              post={heroPost}
-              config={{ showExcerpt: false }}
-              styles={{
-                title: {
-                  wrapper: 'mb-3',
-                  title: 'text-lg lg:text-xl lg:leading-relaxed',
-                },
-              }}
-            />
-            <PostItem
-              key={heroPost.databaseId}
-              post={heroPost}
-              config={{ showExcerpt: false }}
-              styles={{
-                title: {
-                  wrapper: 'mb-3',
-                  title: 'text-lg lg:text-xl lg:leading-relaxed',
-                },
-              }}
-            />
-            <PostItem
-              key={heroPost.databaseId}
-              post={heroPost}
-              config={{ showExcerpt: false }}
-              styles={{
-                title: {
-                  wrapper: 'mb-3',
-                  title: 'text-lg lg:text-xl lg:leading-relaxed',
-                },
-              }}
-            />
-            <PostItem
-              key={heroPost.databaseId}
-              post={heroPost}
-              config={{ showExcerpt: false }}
-              styles={{
-                title: {
-                  wrapper: 'mb-3',
-                  title: 'text-lg lg:text-xl lg:leading-relaxed',
-                },
-              }}
-            />
-          </div>
+        <section className="grid md:grid-cols-3 gap-5 mb-7 md:mb-7">
+          {
+            edges.map(({node}) => 
+              <PostItem
+                key={node.databaseId}
+                post={node}
+                config={{
+                  showExcerpt: false,
+                  imageHeight: 800,
+                  imageWidth: 1000,
+                }}
+                styles={{}}
+              />
+            )
+          }
+            
         </section>
-
-       
-
-        {/* {heroPost && (
-          <HeroPost
-            title={heroPost.title}
-            coverImage={heroPost.featuredImage}
-            date={heroPost.date}
-            author={heroPost.author}
-            slug={heroPost.databaseId}
-            excerpt={heroPost.excerpt}
-          />
-        )} */}
-        {/* {morePosts.length > 0 && <MoreStories posts={morePosts} />} */}
       </Container>
     </Layout>
   );

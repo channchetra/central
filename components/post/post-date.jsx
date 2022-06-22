dfb49404b5726bd852287c61f1d06f9c6af0df53
NySominea <sominea.ny77@gmail.com>
2022-06-22 11:38:28 +0700

    format post date to khmer and ago duration

--- components/post/post-date.jsx
+++ components/post/post-date.jsx
@@ -1,14 +1,23 @@
-import { parseISO, format } from 'date-fns'
+import { parseISO, format, formatDistance, differenceInWeeks } from 'date-fns'
+import { km } from 'date-fns/locale'
 
 export default function PostDate({ dateString, className = '', styles }) {
-  const date = parseISO(dateString)
+  
   const classes = {
     wrapper: '',
     date: '',
     ...styles
   }
+ 
+  const date = parseISO(dateString)
+  const diff = differenceInWeeks(new Date(), date)
+  let displayDate = format(date, 'd LLLL yyyy', { locale: km })
+  if (diff < 1) { 
+    // display text if below 1 week past
+    displayDate = formatDistance(date, new Date(), { addSuffix: true, includeSeconds: true, locale: km })
+  }
 
   return <div className={`${className} ${classes.wrapper}`}>
-    <div dateTime={dateString} className={classes.date}>{format(date, 'LLLL	d, yyyy')}</div>
+    <div dateTime={dateString} className={classes.date}>{ `${displayDate}` }</div>
   </div>
 }

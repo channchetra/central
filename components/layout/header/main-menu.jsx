61e81a285fb77a614274e7032702f8fe5cef49f6
NySominea <sominea.ny77@gmail.com>
2022-06-23 19:21:18 +0700

    refactor menu

---
+++ components/layout/header/main-menu.jsx
@@ -0,0 +1,149 @@
+import { Fragment } from 'react';
+import { Popover, Transition } from '@headlessui/react';
+import { MenuIcon, SearchIcon, XIcon } from '@heroicons/react/outline';
+import Image from 'next/image';
+import MainMenuItem from './main-menu-item';
+
+export default function MainMenu({ mainMenus }) {
+  return (
+    <div className="relative -mx-5 md:-mx-0 flex items-center justify-between bg-gradient-to-r from-ams-red to-ams-blue md:from-inherit">
+      <Popover>
+        <div className="md:hidden">
+          <Popover.Button className="rounded-md p-2 inline-flex items-center justify-center text-gray-400 focus:outline-none">
+            <span className="sr-only">Open menu</span>
+            <MenuIcon className="h-9 w-9 text-white" aria-hidden="true" />
+          </Popover.Button>
+        </div>
+        <Popover.Group as="nav" className="hidden md:flex gap-5">
+          {mainMenus.map((menu, mainMenuIndex) =>
+            menu.subMenus ? (
+              <MainMenuItem
+                menu={menu}
+                index={mainMenuIndex}
+                key={`main-menu-${mainMenuIndex}`}
+              />
+            ) : (
+              <a
+                key={`main-menu-${mainMenuIndex}`}
+                href={menu.href}
+                className="group py-4 text-xs lg:text-base text-black font-bold inline-flex items-center focus:outline-0 border-b-2 border-transparent hover:border-b-2 hover:border-sky-400"
+              >
+                {menu.name}
+              </a>
+            )
+          )}
+        </Popover.Group>
+
+        <Transition
+          as={Fragment}
+          enter="duration-200 ease-out"
+          enterFrom="opacity-0 scale-95"
+          enterTo="opacity-100 scale-100"
+          leave="duration-100 ease-in"
+          leaveFrom="opacity-100 scale-100"
+          leaveTo="opacity-0 scale-95"
+        >
+          <Popover.Panel
+            focus
+            className="absolute top-0 inset-x-0 z-10 transition transform origin-top-right md:hidden"
+          >
+            <div className="shadow-lg ring-1 ring-black ring-opacity-5 bg-white divide-y-2 divide-gray-50">
+              <div className="pt-3 pb-6 px-5">
+                <div className="flex items-center justify-start">
+                  <div className="-mr-2">
+                    <Popover.Button className="bg-white rounded-md p-2 inline-flex items-center justify-center text-gray-400">
+                      <span className="sr-only">Close menu</span>
+                      <XIcon className="h-6 w-6" aria-hidden="true" />
+                    </Popover.Button>
+                  </div>
+                </div>
+                <div className="mt-6">
+                  <nav className="grid grid-cols-1 gap-7">
+                    {mainMenus.map((menu, mainMenuIndex) => (
+                      <a
+                        key={`main-menu-${mainMenuIndex}`}
+                        href={menu.href}
+                        className="-m-3 p-3 flex items-center rounded-lg hover:bg-gray-50"
+                      >
+                        {menu.name}
+                      </a>
+                    ))}
+                  </nav>
+                </div>
+              </div>
+            </div>
+          </Popover.Panel>
+        </Transition>
+      </Popover>
+      <div className="mobile-logo">
+        <div className="w-14 sm:hidden relative">
+          <Image
+            layout="fill"
+            alt=""
+            src="https://asset.ams.com.kh/central/media/AMS-Central-Page-Profile%404x.png"
+            objectFit="contain"
+          />
+        </div>
+      </div>
+
+      <Popover className="sm:hidden">
+        <div className="flex justify-between items-center sm:px-6 md:justify-start">
+          <div className="md:hidden">
+            <Popover.Button className="rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none">
+              <span className="sr-only">Open menu</span>
+              <SearchIcon className="h-7 w-7 text-white" aria-hidden="true" />
+            </Popover.Button>
+          </div>
+        </div>
+
+        <Transition
+          as={Fragment}
+          enter="duration-200 ease-out"
+          enterFrom="opacity-0 scale-95"
+          enterTo="opacity-100 scale-100"
+          leave="duration-100 ease-in"
+          leaveFrom="opacity-100 scale-100"
+          leaveTo="opacity-0 scale-95"
+        >
+          <Popover.Panel
+            focus
+            className="search-form absolute top-0 inset-x-0 z-10 h-screen transition transform origin-top-right md:hidden"
+          >
+            <div className="shadow-lg ring-1 h-full ring-black ring-opacity-5 bg-white flex flex-col">
+              <div className="pt-5 px-5 flex-1 mb-auto">
+                <div className="flex items-center justify-between">
+                  <div />
+                  <div className="-mr-2">
+                    <Popover.Button className="bg-white rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:text-gray-500 hover:bg-gray-100 ">
+                      <span className="sr-only">Close menu</span>
+                      <XIcon className="h-6 w-6" aria-hidden="true" />
+                    </Popover.Button>
+                  </div>
+                </div>
+              </div>
+              <div className="pb-6 px-5 flex-1">
+                <div className="mt-3 h-full">
+                  <input
+                    type="text"
+                    className="
+                        h-14
+                        px-5
+                        mt-1
+                        block
+                        w-full
+                        rounded-md
+                        bg-gray-100
+                        border-transparent
+                        focus:border-gray-500 focus:bg-white focus:ring-0
+                      "
+                    placeholder="ស្វែងរក"
+                  />
+                </div>
+              </div>
+            </div>
+          </Popover.Panel>
+        </Transition>
+      </Popover>
+    </div>
+  );
+}
---
+++ components/layout/header/top-menu.jsx
@@ -0,0 +1,152 @@
+import { Fragment, useState } from 'react';
+import { Popover, Transition } from '@headlessui/react';
+import { SearchIcon } from '@heroicons/react/outline';
+import { ChevronDownIcon } from '@heroicons/react/solid';
+import Link from 'next/link';
+import Image from 'next/image';
+import classNames from 'classnames';
+
+export default function TopMenu({ flagshipMenus, amsWebsites }) {
+  const [popoverOpen, setPopoverOpen] = useState(false);
+
+  return (
+    <div className="hidden sm:flex items-center justify-between">
+      <div className="flex gap-3 items-center">
+        <Link href="/">
+          <a aria-label="AMS Central">
+            <span className="sr-only">AMS Central</span>
+            <div className="h-6 w-20 sm:h-8 relative">
+              <Image
+                layout="fill"
+                alt="AMS Central"
+                src="https://asset.ams.com.kh/central/media/APSARA_MEDIA_SERVICES_SECONDARY_LOGO.png"
+                objectFit="contain"
+              />
+            </div>
+          </a>
+        </Link>
+        <Link href="/">
+          <a
+            aria-label="CENTRAL"
+            className="text-3xl leading-none before:content-['|'] before:mr-3"
+            style={{ fontFamily: 'Battambang' }}
+          >
+            CENTRAL
+          </a>
+        </Link>
+        <Popover className="relative bg-white">
+          <div className="max-w-screen-xl container mx-auto">
+            <div className="flex items-center md:justify-start md:space-x-10 z">
+              <Popover.Group
+                as="nav"
+                className="ml-auto hidden md:flex space-x-10"
+              >
+                <Popover className="relative">
+                  {({ open }) => (
+                    <>
+                      <Popover.Button
+                        onMouseEnter={() => setPopoverOpen(true)}
+                        onMouseLeave={() => setPopoverOpen(false)}
+                        className={classNames(
+                          open ? 'text-gray-900' : 'text-gray-500',
+                          'group bg-white rounded-md inline-flex items-center text-base font-medium hover:text-gray-900 focus:outline-none py-3'
+                        )}
+                      >
+                        <ChevronDownIcon
+                          className={classNames(
+                            open ? 'text-gray-600' : 'text-gray-400',
+                            'h-5 w-5 group-hover:text-gray-500'
+                          )}
+                          aria-hidden="true"
+                        />
+                      </Popover.Button>
+
+                      {popoverOpen && (
+                        <Transition
+                          show={popoverOpen}
+                          onMouseEnter={() => setPopoverOpen(true)}
+                          onMouseLeave={() => setPopoverOpen(false)}
+                          as={Fragment}
+                          enter="transition ease-out duration-200"
+                          enterFrom="opacity-0 translate-y-1"
+                          enterTo="opacity-100 translate-y-0"
+                          leave="transition ease-in duration-150"
+                          leaveFrom="opacity-100 translate-y-0"
+                          leaveTo="opacity-0 translate-y-1"
+                        >
+                          <Popover.Panel className="absolute z-20 -ml-4 transform px-2 sm:px-0 lg:ml-0 lg:left-1/2 lg:-translate-x-1/2 shadow-md">
+                            <div className="rounded-lg overflow-hidden">
+                              <div className="relative grid gap-6 bg-white p-5 sm:gap-5 sm:p-3">
+                                {amsWebsites.map((item, index) => (
+                                  <a
+                                    key={`ams-website-${index}`}
+                                    href={item.href}
+                                    className="-m-3 p-3 flex items-start rounded-lg whitespace-nowrap hover:bg-gray-50 text-sm"
+                                  >
+                                    <div className="ml-4 text-sm font-medium text-gray-900">
+                                      {item.name}
+                                    </div>
+                                  </a>
+                                ))}
+                              </div>
+                            </div>
+                          </Popover.Panel>
+                        </Transition>
+                      )}
+                    </>
+                  )}
+                </Popover>
+              </Popover.Group>
+            </div>
+          </div>
+        </Popover>
+      </div>
+      <div className="flex gap-2">
+        <div className="flagship items-center hidden sm:flex">
+          {flagshipMenus.map((item, flagShipIndex) => (
+            <a
+              key={`flag-ship-${flagShipIndex}`}
+              href={item.href}
+              className="p-4 lg:p-5 -skew-x-12 text-xs lg:text-base font-bold text-white hover:opacity-75 flex items-center h-full"
+            >
+              {item.name}
+            </a>
+          ))}
+        </div>
+        <div className="relative hidden md:flex items-center justify-end">
+          <Popover>
+            <Popover.Button className="-ml-2 py-3 lg:py-5 px-2 lg:px-4 -skew-x-12 inline-flex items-center justify-center text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none">
+              <span className="sr-only">Open search</span>
+              <SearchIcon className="h-6 w-6 skew-x-12" aria-hidden="true" />
+            </Popover.Button>
+
+            <Transition
+              as={Fragment}
+              enter="duration-200 ease-out"
+              enterFrom="opacity-0 scale-95"
+              enterTo="opacity-100 scale-100"
+              leave="duration-100 ease-in"
+              leaveFrom="opacity-100 scale-100"
+              leaveTo="opacity-0 scale-95"
+            >
+              <Popover.Panel
+                focus
+                className="search-form absolute z-40 -ml-4 transform w-72 lg:ml-0 lg:left-1/2 md:-translate-x-[80%] opacity-100 translate-y-0"
+              >
+                <div className="rounded-lg shadow-lg ring-1 h-full ring-black ring-opacity-5 bg-white flex flex-col">
+                  <div className="p-3 flex-1">
+                    <input
+                      type="text"
+                      className="h-12 mt-1 px-3 block w-full rounded-md bg-gray-100 border-transparent focus:outline-none"
+                      placeholder="ស្វែងរក"
+                    />
+                  </div>
+                </div>
+              </Popover.Panel>
+            </Transition>
+          </Popover>
+        </div>
+      </div>
+    </div>
+  );
+}

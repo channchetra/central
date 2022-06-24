5603a8147e176a99633261ca8c51d0959d5f5805
sokmean-mptc <ngonsokmean@mptc.gov.kh>
2022-06-24 15:05:41 +0700

    #

--- pages/posts.jsx
+++ pages/posts.jsx
@@ -1,23 +1,24 @@
 import TemplateArchive from '~/templates/archive';
-import { getPaginatedPosts } from '../lib/posts';
+import { getAllPosts } from '../lib/posts';
 
-export default function Posts({ posts, pagination }) {
-  // console.warn(posts, pagination);
-  
-  return <TemplateArchive posts={posts} pagination={pagination} />;
+export default function Posts({posts}) {
+  const attr = {
+    pageTitle: {
+      description: 'កម្មវិធី AVI VOICE on AMS ជាកម្មវិធីថ្មីមួយ ដែលត្រូវបានបង្កើតឡើងក្រោមកិច្ចសហការរវាង AMS និង AVI (វិទ្យាស្ថានចក្ខុវិស័យអាស៊ី) ក្នុងគោលបំណងចែករំលែកនូវចំណេះដឹង និងការវិភាគដល់ប្រិយមិត្តអ្នកស្តាប់ ជាពិសេសយុវជន លើប្រធានបទសំខាន់ៗ ដូចជាភូមិសាស្រ្តនយោបាយនិងសេដ្ឋកិច្ចសកលនិងក្នុងតំបន់ ការអភិវឌ្ឍដោយចីរភាព សេដ្ឋកិច្ចឌីជីថល និងនវានុវត្តន៍បច្ចេកវិទ្យា ដើម្បីរួមចំណែកក្នុងការអភិវឌ្ឍនូវសមត្ថភាព និងការយល់ដឹងរបស់យុវជនកម្ពុជាជំនាន់ថ្មី'
+    }
+  }
+  return <TemplateArchive posts={posts} attributes={attr} />;
 }
 
 export async function getStaticProps() {
-  const { posts, pagination } = await getPaginatedPosts({
-    queryIncludes: 'archive',
-  });
+  const options = {
+    queryIncludes: 'archive'
+  } 
+  const {posts} = await getAllPosts( options );
   return {
-    props: {
-      posts,
-      pagination: {
-        ...pagination,
-        basePath: '/posts',
-      },
+    props: { 
+      posts
     },
+    revalidate: 10,
   };
 }
--- templates/archive.jsx
+++ templates/archive.jsx
@@ -1,35 +1,66 @@
-import Layou from 'components/Layou';
-import Pagination from '~/components/Pagination';
+import InfiniteScroll from 'react-infinite-scroller';
+import CommonLoader from '~/components/common/loader';
+import Container from '~/components/container';
+import PageBanner from '~/components/page/page-banner';
+import PageTitle from '~/components/page/page-title';
 import PostItem from '~/components/post/post-item';
+import useStaticInfiniteScroll from '~/hooks/use-static-infinite-scroll';
 
-export default function TemplateArchive({
-  posts,
-  pagination,
-}) {
-
+export default function TemplateArchive({posts, attributes = {}}) {
+  // console.warn(posts)
+  // return null
+  const pageBanner = {
+    image: "https://asset.ams.com.kh/central/media/AMS-Cover-AVI-Voice.jpg",
+    enable: true,
+    ...attributes.pageBanner
+  }
+  const pageTitle = {
+    title: "Archive",
+    description: "Description",
+    image: "https://asset.ams.com.kh/central/media/AVI-Voice-on-AMS.jpg",
+    className: "my-5",
+    config: {
+      showLineSeparator: true
+    },
+    enable: true,
+    ...attributes.pageTitle
+  }
+  const { items, hasMore, loadMore } = useStaticInfiniteScroll(posts);
   return (
-    <Layou>
-      <section className="grid md:grid-cols-2 gap-5 mb-7 md:mb-7">
+    <>
+      { pageBanner.enable &&
+      <PageBanner {...pageBanner} />
+      }
+      <Container>
         {
-          posts.map((node) =>
-            <PostItem
-              key={node.id}
-              post={node}
-              config={{
-                showExcerpt: false,
-              }}
-              styles={{}}
-            />
-          )
+          pageTitle.enable &&
+          <PageTitle {...pageTitle} />
         }
-      </section>
-      {pagination && (
-        <Pagination
-          currentPage={pagination?.currentPage}
-          pagesCount={pagination?.pagesCount}
-          basePath={pagination?.basePath}
-        />
-      )}
-    </Layou>
+        <InfiniteScroll
+          pageStart={0}
+          loadMore={loadMore}
+          hasMore={hasMore}
+          loader={
+            <div className="loader my-5" key={0}>
+              <CommonLoader />
+            </div>
+          }
+        >
+          <section className="grid md:grid-cols-4 gap-5 mb-5">
+            {items.map((post) => (
+              <PostItem
+                key={post.id}
+                post={post}
+                config={{
+                  showExcerpt: false,
+                  showMeta: true,
+                }}
+                styles={{}}
+              />
+            ))}
+          </section>
+        </InfiniteScroll>
+      </Container>
+    </>
   );
 }

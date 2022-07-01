b33634105711ec09c57c1eedae6a34eaf24a26b1
sokmean-mptc <ngonsokmean@mptc.gov.kh>
2022-07-01 11:19:53 +0700

    #

--- pages/detail/[id].jsx
+++ pages/detail/[id].jsx
@@ -1,17 +1,8 @@
-import Image from 'next/image';
-import Link from 'next/link';
 import { useEffect, useState } from 'react';
-import Head from 'next/head';
-import Container from '~/components/container';
 import { getPostById } from '~/lib/posts';
-import sanitizeHtml from 'sanitize-html';
-import InfiniteScroll from 'react-infinite-scroller';
 import CommonLoader from '~/components/common/loader';
-import PostCategoryTag from '~/components/post/post-category-tag';
-import PostDate from '~/components/post/post-date';
-import SectionSeparator from '../../components/section-separator';
+import TemplateSingle from '~/templates/single';
 import { getAllPostsWithSlug } from '../../lib/api';
-import { CMS_NAME } from '../../lib/constants';
 
 export default function Post({ post = {} }) {
 
@@ -63,100 +54,7 @@ export default function Post({ post = {} }) {
   });
 
   return (
-    <Container>
-      <InfiniteScroll
-        pageStart={0}
-        loadMore={previous}
-        hasMore={hasMore}
-        loader={
-          <div className="loader my-5" key={0}>
-            <CommonLoader />
-          </div>
-        }
-      >
-        {posts.map((item) => (
-          <>
-            <article key={item.id} className="mt-4 sm:mt-6">
-              <div id={item.databaseId}> </div>
-              <Head>
-                <title>
-                  {title} {CMS_NAME}
-                </title>
-                <meta
-                  property="og:image"
-                  content={item.featuredImage?.sourceUrl}
-                />
-              </Head>
-              <div className="grid sm:grid-cols-3 gap-3 sm:gap-6">
-                <div className="col-span-2">
-                  <h3 className="entry-title text-lg sm:text-2xl font-bold">
-                    {item.title}
-                  </h3>
-                  <p className="post-date my-3 text-sm">
-                    <PostCategoryTag categories={item.categories} />
-                  </p>
-                  <p className="post-date my-3 text-sm">
-                    {/* <span className="py-1 px-2 text-white bg-ams-purple dark:bg-slate-600"> */}
-                    {/* </span>  */} {/* | {item.author} | {item.date} */}
-                    <PostDate dateString={item.date} />
-                  </p>
-                  <div className="relative my-3 sm:my-6 pb-[56%]">
-                    <Image
-                      src={item.featuredImage?.sourceUrl}
-                      layout="fill"
-                      objectFit="cover"
-                    />
-                  </div>
-                  <div
-                    dangerouslySetInnerHTML={{
-                      __html: sanitizeHtml(item.content, {
-                        allowedTags: sanitizeHtml.defaults.allowedTags.concat([
-                          'img',
-                        ]),
-                      }),
-                    }}
-                  />
-                  <div className="ads relative my-4 sm:my-7 pb-[16%]">
-                    <Image
-                      src="https://asset.ams.com.kh/central/media/ads-olatte.jpg"
-                      layout="fill"
-                      objectFit="cover"
-                    />
-                  </div>
-                  <div className="join-telegram relative my-4 sm:my-7 pb-[25%] border">
-                    <Link href="https://t.me/ApsaraMediaServices">
-                      <a target="_blank">
-                        <Image
-                          src="https://asset.ams.com.kh/central/media/AMS-Telegram-100.jpg"
-                          layout="fill"
-                          objectFit="cover"
-                        />
-                      </a>
-                    </Link>
-                  </div>
-                  {/* {item.author} */}
-                </div>
-                <div>
-                  <div className="sticky top-14">
-                    <div className="ads relative my-4 sm:my-7">
-                      <Image
-                        src="https://asset.ams.com.kh/central/media/cama-mf-2.jpg"
-                        width="100%"
-                        height="100%"
-                        layout="responsive"
-                        objectFit="contain"
-                      />
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </article>
-
-            <SectionSeparator />
-          </>
-        ))}
-      </InfiniteScroll>
-    </Container>
+   <TemplateSingle previous={previous} title={title} hasMore={hasMore} posts={posts} />
   );
 }
 

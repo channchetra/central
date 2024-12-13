1c91829dfc629ad51eae523ea25b280e67b50b45
NySominea <sominea.ny77@gmail.com>
2024-12-13 11:04:35 +0700

    feat: add analytics script

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -5,15 +5,31 @@ export default class MyDocument extends Document {
   render() {
     return (
       <Html lang="en">
-        <Head />
-        <body>
-          <Main />
-          <NextScript />
+        <Head>
           <Script
-            strategy="afterInteractive"
+            strategy="beforeInteractive"
             src="https://analytic.cloud.sovichetra.com/script.js"
             data-website-id="b1e5ed14-aa5d-4f63-ab38-b0023cd61af5"
           />
+          <Script
+            strategy="beforeInteractive"
+            src="https://www.googletagmanager.com/gtag/js?id=G-J7EDZT7KLK"
+          />
+          <Script id="gtag" strategy="beforeInteractive">
+            {`
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){
+                dataLayer.push(arguments);
+                }
+              gtag('js', new Date());
+
+              gtag('config', 'G-J7EDZT7KLK');
+            `}
+          </Script>
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
         </body>
       </Html>
     );
